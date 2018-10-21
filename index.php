84c11502a0f2519cf2ed90d4aa7a20a816ce450b
Vinicius Alencar <viniciusalencardev@gmail.com>
2018-10-21 12:34:18 -0300

    Alterações iniciais no index

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
-echo "Olá, mundo!2";
+
+include_once("config/config.php");
+$pastaPaginas = "paginas/";
+
+$paginaInclude = "";
+
+if(isset($_GET['url'])) {
+	$url = $_GET['url'];
+	if(file_exists($pastaPaginas.$url.".php"))
+		$paginaInclude = $url;
+	else
+		$paginaInclude = "pagina-nao-encontrada";
+} else
+	$paginaInclude = "home";
+
+include_once($paginaInclude.$pastaPaginas.".php");
+	
